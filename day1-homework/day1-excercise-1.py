5d681e70e50307a5742c25c87f75db649910c855
Siran Tian <siran.tian@jhu.edu>
2019-08-26 18:17:04 -0400

    update the answers for question1

--- day1-homework/day1-excercise-1.py
+++ day1-homework/day1-excercise-1.py
@@ -3,10 +3,21 @@
 import sys
 
 f = open (sys.argv[1])
-count = 0
-for lines in f.readlines():
-    count += 1
+count = 0 
+for line in f:
+    fields= line.split("\t")
+    if fields[2] != "*":
+        count += 1
+        
+print (count)    
 
-print (count)
+#count = 0 
+#for item in fields[2]:
+    #if item != "*":
+        #count += 1 
+
+#print (count)
+    
+
 
 
