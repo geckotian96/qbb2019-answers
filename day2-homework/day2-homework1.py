90b14c1618c93cfdeb2b0f73deba1c7d3bca2819
Siran Tian <siran.tian@jhu.edu>
2019-08-27 16:50:35 -0400

    Update the script after fixing FBgn

--- day2-homework/day2-homework1.py
+++ day2-homework/day2-homework1.py
@@ -5,12 +5,11 @@ import sys
 f=open(sys.argv[1])
 
 for i, line in enumerate(f):
-    if "DROME" not in line:
+    if "DROME" and "FBgn" not in line:
         continue
     columns = line.split()
     flybase=columns[-1]
     uniport_name =columns[-2]
     
-    print (flybase, uniport_name)
-    
+    print (flybase,"\t", uniport_name)
         
